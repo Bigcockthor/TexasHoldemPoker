8f78d8b9083f54aada7015d7517647565a3fc5af
CquptZouSheng <382223154@qq.com>
2016-10-14 21:25:28 +0800

    Done.

--- src/Subject.java
+++ src/Subject.java
@@ -12,29 +12,268 @@ public class Subject {
         Scanner mScanner = new Scanner(System.in);
         Perflop(poker, mScanner);
         FlopTurnRiver(poker, mScanner);
+        sortPoint(poker);
+        RoyalFlush(poker);
+        StraightFlush(poker);
+        FourOfAKind(poker);
+        FullHouse(poker);
         Flush(poker);
+        Straight(poker);
+        ThreeOfAKind(poker);
+        Pair(poker);
+        HighCard(poker);
+        System.out.println("Draw!");
     }
 
-   /* private static void sortSuit(Poker[][] poker) {
-        Poker temp;
-        int num1 = 0, num2 = 0;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 6; j++) {
-                for (int k = j + 1; k < 7; k++) {
-                    for (int l = 0; l <4 ; l++) {
-                        if (poker[i][j].suit==pokerSuit[l]){
-                            num1=l;
-                        }
-                        if (poker[i][k].suit==pokerSuit[l]){
-                            num2=l;
-                        }
-                    }
-                    if (num1>num2){
-                        temp=poker[i][j];
-                        poker[i][j]=poker[i][k];
-                        poker[i][k]=temp;
+    private static void FullHouse(Poker[][] poker) {
+        int a[][]=new int[2][13];
+        int b[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <7 ; j++) {
+                for (int k = 0; k <13 ; k++) {
+                    if (poker[i][j].point==pokerPoint[k]){
+                        a[i][k]++;
                     }
                 }
+            }
+            for (int j = 0; j <13 ; j++) {
+                if (a[i][j]==3){
+                    b[i]++;
+                    break;
+                }
+            }
+            for (int j = 0; j <13 ; j++) {
+                if (a[i][j]==2){
+                    b[i]++;
+                    break;
+                }
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+    }
+
+    private static void HighCard(Poker[][] poker) {
+        int b[][]=new int[2][7];
+        for (int i = 0; i <2 ; i++) {
+            int flag;
+            flag = 0;
+            for (int j = 6; j >= 0; j--) {
+                for (int k = 0; k < 13; k++) {
+                    if (poker[i][j].point == pokerPoint[k]) {
+                        b[i][flag] = k;
+                        flag++;
+                    }
+                }
+            }
+        }
+        for (int i = 6; i >=0 ; i--) {
+            if (b[0][i]>b[1][i]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (b[0][i]<b[1][i]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void Pair(Poker[][] poker) {
+        int a[][]=new int[2][13];
+        int b[]=new int[2];
+        int temp[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <7 ; j++) {
+                for (int k = 0; k <13 ; k++) {
+                    if (poker[i][j].point==pokerPoint[k]){
+                        a[i][k]++;
+                    }
+                }
+            }
+            for (int j = 0; j <13 ; j++) {
+                if (a[i][j]==2){
+                    b[i]++;
+                    temp[i]=j;
+                }
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+        if (b[0]==b[1]){
+            if (temp[0]>temp[1]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (temp[0]<temp[1]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void ThreeOfAKind(Poker[][] poker) {
+        int a[][]=new int[2][13];
+        int b[]=new int[2];
+        int temp[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <7 ; j++) {
+                for (int k = 0; k <13 ; k++) {
+                    if (poker[i][j].point==pokerPoint[k]){
+                        a[i][k]++;
+                    }
+                }
+            }
+            for (int j = 0; j <13 ; j++) {
+                if (a[i][j]==3){
+                    b[i]++;
+                    temp[i]=j;
+                }
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+        if (b[0]==b[1]){
+            if (temp[0]>temp[1]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (temp[0]<temp[1]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void Straight(Poker[][] poker) {
+        int b[]=new int[7];
+        int c[]=new int[2];
+        int big[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            int flag;
+            flag=0;
+            for (int j = 6; j>=0  ; j--) {
+                for (int k = 0; k < 13; k++) {
+                    if (poker[i][j].point == pokerPoint[k]) {
+                        b[flag] = k;
+                        flag++;
+                    }
+                }
+            }
+            for (int j = 0; j <3 ; j++) {
+                if (b[j]-1==b[j+1]&&b[j+1]-1==b[j+2]&&b[j+2]-1==b[j+3]&&b[j+3]-1==b[j+4]){
+                    c[i]++;
+                    big[i]=b[j+4];
+                }
+            }
+        }
+        if (c[0]>c[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (c[0]<c[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+        if (c[0]==c[1]&&c[0]==1){
+            if (big[0]>big[1]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (big[0]<big[1]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void Flush(Poker[][] poker) {
+        int a[][]=new int[2][5];
+        int b[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (poker[i][j].suit.equals(pokerSuit[0])) {
+                    a[i][0]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[1])) {
+                    a[i][1]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[2])) {
+                    a[i][2]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[3])) {
+                    a[i][3]++;
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                if (a[i][j] >= 5) {
+                    b[i]++;
+                }
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+    }
+
+    private static void FourOfAKind(Poker[][] poker) {
+        int a[][]=new int[2][13];
+        int b[]=new int[2];
+        int temp[]=new int[2];
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <7 ; j++) {
+                for (int k = 0; k <13 ; k++) {
+                    if (poker[i][j].point==pokerPoint[k]){
+                        a[i][k]++;
+                    }
+                }
+            }
+            for (int j = 0; j <13 ; j++) {
+                if (a[i][j]==4){
+                    b[i]++;
+                    temp[i]=j;
+                }
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+        if (b[0]==b[1]){
+            if (temp[0]>temp[1]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (temp[0]<temp[1]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
             }
         }
     }
@@ -61,42 +300,127 @@ public class Subject {
                 }
             }
         }
-    }*/
+    }
 
-    private static void Flush(Poker[][] poker) {
+    private static void RoyalFlush(Poker[][] poker) {
+        int a[][]=new int[2][5];
+        int b[]=new int[2];
+        String commonSuit=new String();
         for (int i = 0; i <2 ; i++) {
             for (int j = 0; j <7 ; j++) {
-                for (int k = 0; k <13 ; k++) {
-                    if (poker[i][j].point==pokerPoint[k]){
-
-                    }
+                if (poker[i][j].suit.equals(pokerSuit[0])){
+                    a[i][0]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[1])){
+                    a[i][1]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[2])){
+                    a[i][2]++;
+                }
+                if (poker[i][j].suit.equals(pokerSuit[3])){
+                    a[i][3]++;
                 }
             }
+            for (int j = 0; j <4 ; j++) {
+                if (a[i][j]>=5){
+                    commonSuit=pokerSuit[j];
+                }
+                a[i][j]=0;
+            }
+            for (int j = 0; j <7 ; j++) {
+                if (poker[i][j].suit.equals(commonSuit)&&poker[i][j].point=='0'){
+                    a[i][0]=1;
+                }
+                if (poker[i][j].suit.equals(commonSuit)&&poker[i][j].point=='J'){
+                    a[i][1]=1;
+                }
+                if (poker[i][j].suit.equals(commonSuit)&&poker[i][j].point=='Q'){
+                    a[i][2]=1;
+                }
+                if (poker[i][j].suit.equals(commonSuit)&&poker[i][j].point=='K'){
+                    a[i][3]=1;
+                }
+                if (poker[i][j].suit.equals(commonSuit)&&poker[i][j].point=='A'){
+                    a[i][4]=1;
+                }
+            }
+            if (a[i][0]==1&&a[i][1]==1&&a[i][2]==1&&a[i][3]==1&&a[i][4]==1){
+                b[i]++;
+            }
+        }
+        if (b[0]>b[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (b[0]<b[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
         }
     }
 
-    private static void Draw(Poker[][] poker, int n) {
-        int num1 = 0, num2 = 0;
-        for (int i = n; i >= 0; i--) {
-            for (int l = 0; l < 13; l++) {
-                if (poker[0][i].point == pokerPoint[l]) {
-                    num1 = l;
+    private static void StraightFlush(Poker[][] poker) {
+        int a[][]=new int[2][5];
+        int b[]=new int[7];
+        int c[]=new int[2];
+        int big[]=new int[2];
+        String commonSuit=new String();
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (poker[i][j].suit.equals(pokerSuit[0])) {
+                    a[i][0]++;
                 }
-                if (poker[1][i].point == pokerPoint[l]) {
-                    num2 = l;
+                if (poker[i][j].suit.equals(pokerSuit[1])) {
+                    a[i][1]++;
                 }
-                if (num1 > num2) {
-                    System.out.println("Player 1 win!");
-                    System.exit(0);
+                if (poker[i][j].suit.equals(pokerSuit[2])) {
+                    a[i][2]++;
                 }
-                if (num2 > num1) {
-                    System.out.println("Player 2 win!");
-                    System.exit(0);
+                if (poker[i][j].suit.equals(pokerSuit[3])) {
+                    a[i][3]++;
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                if (a[i][j] >= 5) {
+                    commonSuit = pokerSuit[j];
+                }
+            }
+            int flag;
+            flag=0;
+            for (int j = 6; j>=0  ; j--) {
+                if (poker[i][j].suit.equals(commonSuit)){
+                    for (int k = 0; k <13 ; k++) {
+                        if (poker[i][j].point==pokerPoint[k]){
+                            b[flag]=k;
+                            flag++;
+                        }
+                    }
+                }
+            }
+            for (int j = 0; j <3 ; j++) {
+                if (b[j]-1==b[j+1]&&b[j+1]-1==b[j+2]&&b[j+2]-1==b[j+3]&&b[j+3]-1==b[j+4]){
+                    c[i]++;
+                    big[i]=b[j+4];
                 }
             }
         }
-        System.out.println("Draw!");
-        System.exit(0);
+        if (c[0]>c[1]){
+            System.out.println("Player 1 win!");
+            System.exit(0);
+        }
+        if (c[0]<c[1]){
+            System.out.println("Player 2 win!");
+            System.exit(0);
+        }
+        if (c[0]==c[1]&&c[0]==1){
+            if (big[0]>big[1]){
+                System.out.println("Player 1 win!");
+                System.exit(0);
+            }
+            if (big[0]<big[1]){
+                System.out.println("Player 2 win!");
+                System.exit(0);
+            }
+        }
     }
 
     private static void FlopTurnRiver(Poker[][] poker, Scanner mScanner) {
@@ -135,11 +459,11 @@ public class Subject {
                 try {
                     card.charAt(k + 2);
                 } catch (IndexOutOfBoundsException e) {
-                    tempCard = new Poker(String.valueOf(temp), card.charAt(k + 1));
+                    tempCard = new Poker(new String(temp).substring(0,k), card.charAt(k + 1));
                     flag = false;
                 }
                 if (flag) {
-                    tempCard = new Poker(String.valueOf(temp), '0');
+                    tempCard = new Poker(new String(temp).substring(0,k), '0');
                 }
                 poker[i][j] = tempCard;
             }
